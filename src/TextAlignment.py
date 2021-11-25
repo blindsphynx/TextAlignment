625ebf78d21bcc5ab9f7eb035fa3949619b3cc1b
volkaufff <81808301+blindsphynx@users.noreply.github.com>
2021-11-26 02:20:37 +0300

    add LEFT Alignment

---
+++ src/TextAlignment.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class Alignment(Enum):
+    LEFT = 0,
+    RIGHT = 1,
+    CENTER = 2,
+    JUSTIFY = 3
+
+
+def alignText(text, alignment=Alignment.LEFT) -> str:
+    if Alignment.LEFT:
+        while text[0] == " ":
+            text = text[1:]
+    return text
+
+
+# print(alignText('     akj lk pwmm'))
