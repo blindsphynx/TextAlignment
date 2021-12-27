4fea6cbabb44bf8e9cf79dd77f036eb25ed2cb8a
blindsphynx <violakruglikova98683@gmail.com>
2021-12-27 13:40:00 +0300

    JUSTIFY alignment and comments added

--- src/TextAlignment.py
+++ src/TextAlignment.py
@@ -17,8 +17,8 @@ def alignText(text, alignment=Alignment.LEFT, textWidth=120) -> str:
         return '\n'.join(alignmentRight(widthFormattedText, textWidth))
     if alignment == Alignment.CENTER:
         return '\n'.join(alignmentCenter(widthFormattedText, textWidth))
-    # if alignment == Alignment.JUSTIFY:
-    #     return '\n'.join(alignmentJustify(widthFormattedText, textWidth))
+    if alignment == Alignment.JUSTIFY:
+        return '\n'.join(alignmentJustify(widthFormattedText, textWidth))
 
 
 def alignmentLeft(widthFormattedText, textWidth):
@@ -65,48 +65,83 @@ def alignmentCenter(widthFormattedText, textWidth):
     return localWidthFormattedText
 
 
-# def alignmentJustify(widthFormattedText, textWidth):
-#     pass
+def alignmentJustify(widthFormattedText, textWidth):
+    localWidthFormattedText = list(widthFormattedText)
+    for i in range(len(localWidthFormattedText)):
+        line = localWidthFormattedText[i]
+        if localWidthFormattedText[0] == ' ':                                    # cut spaces on the right
+            line = line[1:]
+        if localWidthFormattedText[-1] == ' ':                                   # cut spaces on the left
+            line = line[:-1]
+
+        countOfWords = len(line.split(' '))                                      # number of words in a line
+        tempLine = ""
+
+        if countOfWords == 1:                                                    # if a line has only one word
+            tempLine = localWidthFormattedText[i].split(' ')[0]
+            localWidthFormattedText[i] = tempLine
+            continue
+
+        spacesBetweenWords = textWidth                                           # count available places for spaces
+
+        for word in localWidthFormattedText[i].split(' '):
+            spacesBetweenWords -= len(word)
+
+        needSpaces = spacesBetweenWords // (countOfWords - 1)                    # how many spaces to add between words
+        extraSpaces = spacesBetweenWords % (countOfWords - 1)                    # extra spaces (when the number is odd)
+
+        for word in localWidthFormattedText[i].split(' ')[:countOfWords - 1]:    # start to put it all together
+            if extraSpaces > 0:
+                tempLine += word
+                for j in range(needSpaces + 1):
+                    tempLine += ' '
+                    extraSpaces -= 1
+            else:
+                tempLine += word
+                for j in range(needSpaces):
+                    tempLine += ' '
+
+        localWidthFormattedText[i] = tempLine + localWidthFormattedText[i].split(' ')[countOfWords - 1]
+    return localWidthFormattedText
 
 
-def getWidthFormattedText(text, textWidth) -> str:
-    def transferWord(_tempWord):
-        tempIt = textWidth - len(newText[-1])
-        newText[len(newText) - 1] += _tempWord[:tempIt]
+def getWidthFormattedText(text, textWidth) -> str:          # this function makes every line as long as textWidth
+    def transferWord(_tempWord):                            # this function transfers a part of a word to the next line
+        tempIt = textWidth - len(newText[-1])               # width of a line without the last word
+        newText[len(newText) - 1] += _tempWord[:tempIt]     # add a new word cut to the end of a line
         newText.append("")
-        if len(_tempWord[tempIt:]) > textWidth:
-            return transferWord(_tempWord[tempIt:])
-        else:
-            return _tempWord[tempIt:]
+        if len(_tempWord[tempIt:]) > textWidth:             # if this new word is longer than width
+            return transferWord(_tempWord[tempIt:])         # transfer it
+        return _tempWord[tempIt:]
 
     def spaceAtTheEndOfAWord():
-        if len(newText[-1]) < textWidth:
-            newText[len(newText) - 1] += " "
+        if len(newText[-1]) < textWidth:                    # if the last word is shorter than a possible line
+            newText[len(newText) - 1] += " "                # add space at the end
         else:
             newText.append("")
 
     if textWidth <= 0:
         raise Exception("textWidth less than or equal to 0")
 
-    newText = [""]
+    newText = [""]                                          # to keep processed words
 
-    oneStringText = text.replace('\n', ' ')
+    oneStringText = text.replace('\n', ' ')                 # convert our text into one string
 
-    for word in oneStringText.split(' '):
-        tempWord = word
+    for word in oneStringText.split(' '):                   # convert this string into a list of separate words
+        tempWord = word                                     # in order to be able to use a word itself, not as iterator
 
-        if len(word) > textWidth:
-            temp = transferWord(tempWord)
+        if len(word) > textWidth:                           # if a word is longer than width of a line
+            temp = transferWord(tempWord)                   # transfer a part of a word
 
-            newText[-1] += temp
-            spaceAtTheEndOfAWord()
+            newText[-1] += temp                             # add the first part of a transferred word to newText
+            spaceAtTheEndOfAWord()                          # add space if needed
 
-        elif len(word) > textWidth - len(newText[-1]):
-            newText.append(tempWord)
+        elif len(word) > textWidth - len(newText[-1]):      # if a word is longer than width without the last newText ->
+            newText.append(tempWord)                        # element, add a word to newText
             spaceAtTheEndOfAWord()
 
         else:
-            newText[len(newText) - 1] += tempWord
+            newText[len(newText) - 1] += tempWord           # add a word at the end of newText
             spaceAtTheEndOfAWord()
     return newText
 
@@ -124,4 +159,20 @@ def getWidthFormattedText(text, textWidth) -> str:
 # сделали эмбриональные клетки устойчивыми к молекулярному сигналу. После этого
 # ящерицы смогли восстановить нормальный хвост уже во взрослом возрасте.
 # Теперь исследователи хотят усовершенствовать свой метод и использовать его для
-# заживления глубоких и трудных ран.""", alignment=Alignment.CENTER, textWidth=30))
+# заживления глубоких и трудных ран.""", alignment=Alignment.JUSTIFY, textWidth=30))
+
+# print(alignText(text="""Аналитики Яндекс Go изучили статистику заказов такси в предновогодний вечер,
+# ночь и утро 1 января в прошлые годы и поделились рекомендациями, в какое время поездки будут более выгодными.
+# Цена на такси зависит от множества факторов: количества свободных машин в районе, где находится пользователь,
+# числа желающих заказать такси, пробок, тарифа, расстояния и длительности дороги. Чем больше шоферов готовы принять
+# заказ, тем ниже будет стоимость. И наоборот – чем меньше свободных водителей на линии, тем дороже может
+# оказаться поездка. Лучше всего 31 декабря заказывать такси до 20:00 или начиная с 22:00. Вечером большинство людей
+# уже успели завершить поездки – сделали последние покупки или добрались в гости. То есть свободных машин больше,
+# такси приедет быстрее, стоимость поездки будет ниже. Но стоит поторопиться, потому что ближе к полуночи водители
+# завершают смены, чтобы успеть к семьям за новогодний стол. После боя курантов и до 2:00 ночи 1 января многие люди
+# не сидят дома и путешествуют из гостей в гости или ездят к главным городским ёлкам. Таксистов на линии по-прежнему
+# меньше, чем в обычную ночь, а спрос на этот вид транспорта начинает расти. Так что в этот период оно может стоить
+# чуть дороже. С 2:00 до 4:00 утра — наиболее удачное время для того, чтобы отправиться в гости, погулять по городу или,
+# наоборот, вернуться домой: спрос уже не так высок, и стоимость поездки снижается. Утром 1 января заказывать такси
+# лучше с 08:00 утра: в это время водители начинают выходить на линию, а большинство гуляющих к этому моменту уже
+# вернулись домой.""", alignment=Alignment.JUSTIFY, textWidth=1))
